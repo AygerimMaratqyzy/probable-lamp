280dee39382c261f976be56cbb93de19456b917d
AygerimMaratqyzy <163712638+AygerimMaratqyzy@users.noreply.github.com>
2026-02-06 17:19:28 +0500

    Fix discipline issues condition in scholarship logic

--- practice2/if-else/if_elif_else.py
+++ practice2/if-else/if_elif_else.py
@@ -2,7 +2,7 @@ gpa = float(input("GPA:"))
 income = int(input("Family income: "))
 disc_issues = input("Discipline issues? (yes/no) :")
 
-if disc_issues:
+if disc_issues == "yes":
     print("Scolarship denied")
 elif gpa >= 3.7 and income < 500000:
     print("Full scolarship")
